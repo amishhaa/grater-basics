d2c5efbee10ccba1fd5843767ff1c77a219ae54e
Amisha Chhajed <amishhhaaaa@gmail.com>
2026-02-14 17:01:18 +0530

    cmd: fix module.txt initialisation
    modules are intialised with a boilerplate, which shouldnt happen
    until we specify which modules to initialise.
    
    remove the logic to initialise boilerplate modules in module.txt,
    eventually moving towards garter find.

--- cmd/prepare.go
+++ cmd/prepare.go
@@ -8,29 +8,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// prepareCmd represents the prepare command
 var prepareCmd = &cobra.Command{
 	Use:   "prepare",
 	Short: "Prepare grater workspace",
 	Long:  "Creates a .grater directory and generates required files",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		// 1. Create .grater directory
 		wsDir := ".grater"
 		err := os.MkdirAll(wsDir, 0755)
 		if err != nil {
 			return err
 		}
 
-		// 2. Create modules.txt inside .grater
 		modulesPath := filepath.Join(wsDir, "modules.txt")
-
-		content := []byte("moduleA\nmoduleB\nmoduleC\n")
-
-		err = os.WriteFile(modulesPath, content, 0644)
-		if err != nil {
-			return err
-		}
 
 		fmt.Println("✅ .grater workspace created")
 		fmt.Println("✅ modules.txt generated at", modulesPath)
